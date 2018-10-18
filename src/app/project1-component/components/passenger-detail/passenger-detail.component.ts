2359374b68cd50a1c958f6347aa84342b6f779cc
yapcheeann <yapcheeann@gmail.com>
2018-10-18 16:37:33 +0800

    Emitting changes with @Output and EventEmitter

--- src/app/project1-component/components/passenger-detail/passenger-detail.component.ts
+++ src/app/project1-component/components/passenger-detail/passenger-detail.component.ts
@@ -1,26 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-detail',
   styleUrls: ['passenger-detail.component.scss'],
-  template: `
-    <div>
-      <span class="status" [class.checked-in]="detail.checkedIn"></span>
-      {{ detail.fullname }}
-      <div class="date">
-        Check in date: 
-        {{ detail.checkInDate ? (detail.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
-      </div>
-      <div class="children">
-        Children: {{ detail.children?.length || 0 }}
-      </div>
-    </div>
-  `
+  templateUrl: './passenger-detail.component.html'
 })
 export class PassengerDetailComponent {
   @Input()
   detail: Passenger;
+
+  @Output()
+  edit: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  remove: EventEmitter<any> = new EventEmitter();
+
+  editing: boolean = false;
+
   constructor() {}
+
+  onNameChange(value: string) {
+    console.log('Value', value);
+  }
+
+  toggleEdit(){
+    if(this.editing){
+      this.edit.emit(this.detail);
+    }
+    this.editing = !this.editing;
+  }
+
+  onRemove(){
+    this.remove.emit(this.detail);
+  }
 }
--- src/app/project1-component/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/project1-component/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -12,7 +12,9 @@ import { Passenger } from '../../models/passenger.interface';
       </passenger-count>
       <passenger-detail
         *ngFor="let passenger of passengers;"
-        [detail]="passenger">
+        [detail]="passenger"
+        (edit)="handleEdit($event)"
+        (remove)="handleRemove($event)">
       </passenger-detail>
     </div>
   `
@@ -52,5 +54,13 @@ export class PassengerDashboardComponent implements OnInit {
       checkInDate: null,
       children: null
     }];
+  }
+
+  handleRemove(event){
+    console.log(event);
+  }
+
+  handleEdit(event){
+    console.log(event);
   }
 }
