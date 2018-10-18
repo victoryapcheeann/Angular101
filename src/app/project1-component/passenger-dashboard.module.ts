5a19831b03278197c29bf6d2df8439d087752892
yapcheeann <yapcheeann@gmail.com>
2018-10-18 15:13:41 +0800

    Presentational (dumb) components

--- src/app/project1-component/passenger-dashboard.module.ts
+++ src/app/project1-component/passenger-dashboard.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//container
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+
+//stateless components
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
 @NgModule({
   declarations: [
-    PassengerDashboardComponent
+    PassengerDashboardComponent, PassengerCountComponent, PassengerDetailComponent
   ],
   imports: [
     CommonModule
