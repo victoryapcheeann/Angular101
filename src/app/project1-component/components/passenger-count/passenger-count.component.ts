ea2c1deb4e3602d6eb8ed28fd0aa7b86e815bc47
yapcheeann <yapcheeann@gmail.com>
2018-10-18 15:41:58 +0800

    Passing data into components with @Input

--- src/app/project1-component/components/passenger-count/passenger-count.component.ts
+++ src/app/project1-component/components/passenger-count/passenger-count.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-count',
   template: `
     <div>
-      Count component
+      <h3>Airline Passengers</h3>
+      Total Passengers: {{ checkedInCount() }}/{{ items.length }}
     </div>
   `
 })
 export class PassengerCountComponent {
+  @Input()
+  items: Passenger[];
   constructor() {}
+  checkedInCount(): number {
+    if (!this.items) return;
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
+  }
 }
